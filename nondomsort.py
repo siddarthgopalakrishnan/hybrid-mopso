8e900b46d7a060a29dbb80a4e6a95ca78e57aa97
siddarthgopalakrishnan <siddarth.gopalakrishnan@yahoo.in>
2021-04-22 19:55:34 +0530

    Non-dominated sorting O(MN^2) (Open)

--- nondomsort.py
+++ nondomsort.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # Need to improve complexity of this function
+# Current complexity O(M*N^2)
 
 
 def rad_sort(Matrix):
@@ -14,7 +15,6 @@ def rad_sort(Matrix):
     Matrix_temp = Matrix[:, ::-1]  # Swap columns
     Matrix_row = Matrix_temp.T  # Transpose
     rank = np.lexsort(Matrix_row)  # Radix sort
-    # rank holds the indices
     Sorted_Matrix = Matrix[rank, :]  # Sorted matrix
     return Sorted_Matrix, rank
 
@@ -28,59 +28,43 @@ def nondomsort_(violations_, fitness_, pop_size):
     :param fitness_: Fitness values of particles
     :param pop_size: Population size
     :variable NDfront: Non-dominated front
-    :variable pareto_level: 1 - best pareto level
+    :variable pareto_level: pareto ranking of particle
     """
-    # particles, num_of_objective functions
+    # M always = 2 for 2 objective functions
     N, M = fitness_.shape
     # non dominated front
     NDfront = np.inf * np.ones((1, N))
-    pareto_level = 0
+    pareto_level = 1
     # radix sort fitness values
     fitness_, rank = rad_sort(fitness_)
     # Check constraint violation
     violations = violations_[rank]
-    # update non dominated front
-    temp = np.sum(NDfront != np.inf)
-    while(temp < pop_size):
-        pareto_level += 1
-        # for all fitness function value pairs
-        for i in range(N):
-            # print('i loop NDfront:', NDfront)
-            if NDfront[0, i] == np.inf:
-                dom_flag = False
-                for j in range(i - 1, -1, -1):
-                    # print('j loop NDfront:', NDfront)
-                    if NDfront[0, j] == pareto_level:
-                        if violations[i, 0] == True and violations[j, 0] == True:
-                            m = 2
-                            while (m <= M) and (fitness_[i, m-1] >= fitness_[j, m-1]):
-                                m += 1
-                            if m > M:
-                                dom_flag = True
-                            if dom_flag or (M == 2):
-                                break
-                        elif violations[i, 0] == False and violations[i, 1] > (M+1)//2:
-                            NDfront[0, i] = -1
-                            break
-                        elif violations[j, 0] == False and violations[j, 1] > (M+1)//2:
-                            NDfront[0, j] = -1
-                            continue
-                        elif violations[i, 0] == False and violations[j, 0] == False:
-                            if violations[i, 1] > violations[j, 1]:
-                                NDfront[0, i] = -1
-                                break
-                            else:
-                                NDfront[0, j] = -1
-                                continue
-                        elif violations[i, 0] == True and violations[j, 0] == False:
-                            NDfront[0, j] = -1
-                            continue
-                        elif violations[j, 0] == True and violations[i, 0] == False:
-                            NDfront[0, i] = -1
-                            break
-                if not dom_flag:
-                    NDfront[0, i] = pareto_level
-        temp = np.sum(NDfront != np.inf)
+    # O(M*N^2) loop
+    for i in range(N):
+        dom_flag = False
+        # Skipping the particle if it violates constraints
+        if violations[i, 0] == False:
+            continue
+        for j in range(i-1, -1, -1):
+            if NDfront[0, j] == pareto_level:
+                if violations[i, 0] and violations[j, 0]:
+                    if fitness_[i, 1] >= fitness_[j, 1]:
+                        # point i is more than point j
+                        dom_flag = True
+                    break
+                # elif violations[i, 0] == False and violations[j, 0] == False:
+                #     if violations[i, 1] > violations[j, 1]:
+                #         NDfront[0, i] = -1
+                #         break
+                #     else:
+                #         NDfront[0, j] = -1
+                #         continue
+                elif violations[i, 0]:
+                    NDfront[0, j] = -1
+                    continue
+        if dom_flag == False:
+            # First elements always has pareto_level 1
+            NDfront[0, i] = pareto_level
     front_temp = np.zeros((1, N))
     front_temp[0, rank] = NDfront
     return front_temp, pareto_level
