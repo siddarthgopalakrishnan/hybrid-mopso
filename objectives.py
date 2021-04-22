1c0e205e7c504609f120d3efbd4da2e7fce1e19c
siddarthgopalakrishnan <siddarth.gopalakrishnan@yahoo.in>
2021-04-22 19:50:50 +0530

    Objective functions (changed ZDT2 dims)

--- objectives.py
+++ objectives.py
@@ -14,7 +14,7 @@ def get_objective(flag, funct, num_obj, Input):
     if flag == "init":
         if funct == 1:
             # ZDT2
-            dim = 6
+            dim = 30
             MaxValue = np.ones((1, dim))
             MinValue = np.zeros((1, dim))
         elif funct == 2:
@@ -32,36 +32,35 @@ def get_objective(flag, funct, num_obj, Input):
         bounds = np.vstack((MinValue, MaxValue))
         return bounds
     elif flag == "value":
-        Population = Input
-        FunctionValue = np.zeros((Population.shape[0], num_obj))
+        FunctionValue = np.zeros((Input.shape[0], num_obj))
         if funct == 1:
             # ZDT2
-            FunctionValue[:, 0] = Population[:, 0]
+            FunctionValue[:, 0] = Input[:, 0]
             c = np.sum(FunctionValue[:, 1:], axis=1)
-            g = 1.0 + 9.0 * c / 5
+            g = 1.0 + 9.0 * c / 29
             FunctionValue[:, 1] = g * (1 - ((FunctionValue[:, 0] / g)**2))
         elif funct == 2:
             # Tanaka
-            FunctionValue[:, 0] = Population[:, 0]
-            FunctionValue[:, 1] = Population[:, 1]
+            FunctionValue[:, 0] = Input[:, 0]
+            FunctionValue[:, 1] = Input[:, 1]
         elif funct == 3:
             # Osyczka
             FunctionValue[:, 0] = -25 * \
-                ((Population[:, 0]-2)**2) - \
-                ((Population[:, 1]-2)**2) - \
-                ((Population[:, 2]-1)**2) - \
-                ((Population[:, 3]-4)**2) - \
-                ((Population[:, 4]-1)**2)
-            FunctionValue[:, 1] = (Population[:, 0] ** 2) + \
-                (Population[:, 1] ** 2) + \
-                (Population[:, 2] ** 2) + \
-                (Population[:, 3] ** 2) + \
-                (Population[:, 4] ** 2) + \
-                (Population[:, 5] ** 2)
+                ((Input[:, 0]-2)**2) - \
+                ((Input[:, 1]-2)**2) - \
+                ((Input[:, 2]-1)**2) - \
+                ((Input[:, 3]-4)**2) - \
+                ((Input[:, 4]-1)**2)
+            FunctionValue[:, 1] = (Input[:, 0] ** 2) + \
+                (Input[:, 1] ** 2) + \
+                (Input[:, 2] ** 2) + \
+                (Input[:, 3] ** 2) + \
+                (Input[:, 4] ** 2) + \
+                (Input[:, 5] ** 2)
         elif funct == 4:
             # Binh and Korn
-            FunctionValue[:, 0] = (4 * (Population[:, 0]**2)) + \
-                (4 * (Population[:, 1]**2))
-            FunctionValue[:, 1] = ((Population[:, 0]-5)**2) + \
-                ((Population[:, 1]-5)**2)
+            FunctionValue[:, 0] = (4 * (Input[:, 0]**2)) + \
+                (4 * (Input[:, 1]**2))
+            FunctionValue[:, 1] = ((Input[:, 0]-5)**2) + \
+                ((Input[:, 1]-5)**2)
         return FunctionValue
